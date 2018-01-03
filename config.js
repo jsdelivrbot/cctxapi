5d266ac67485d259a2f408832277879132e878ff
chefbook recipes <chefbook.recipes@gmail.com>
2018-01-03 14:04:22 -0800

    Final working version

---
+++ config.js
@@ -0,0 +1,74 @@
+module.exports =
+{
+  "API_KEY": "BC4EDAD4-BF68-4723-B8D3-1D7379E0841D",
+  "btceur": {
+		"api": "bitfinex",
+		"pair": "BTC/EUR"
+	},
+	"investment": 1750.0,
+	"portfolio": [
+		{
+			"title": "LTC (LiteCoin)",
+			"symbol": "LTC",
+			"api": "bitfinex",
+			"pair": "LTC/BTC",
+			"q": 1.08305026
+		},
+		{
+			"title": "XRP (Ripple)",
+			"symbol": "XRP",
+			"api": "bitfinex",
+			"pair": "XRP/BTC",
+			"q": 699.2424392
+		},
+		{
+			"title": "IOTA (Iota)",
+			"symbol": "IOTA",
+			"api": "bitfinex",
+			"pair": "IOTA/BTC",
+			"q": 67.06108908
+		},
+		{
+			"title": "ARK (Ark)",
+			"symbol": "ARK",
+			"api": "binance",
+			"pair": "ARK/BTC",
+			"q": 77.47245000
+		},
+		{
+			"title": "ADA (Cardano)",
+			"symbol": "ADA",
+			"api": "binance",
+			"pair": "ADA/BTC",
+			"q": 199.8
+		},
+		{
+			"title": "SALT (Salt)",
+			"symbol": "SALT",
+			"api": "binance",
+			"pair": "SALT/BTC",
+			"q": 10.08990000
+		},
+		{
+			"title": "SMART (SmartCash)",
+			"symbol": "SMART",
+			"api": "hitbtc2",
+			"pair": "SMART/BTC",
+			"q": 1045.207787
+		},
+		{
+			"title": "DMD (Diamond)",
+			"symbol": "DMD",
+			"api": "livecoin",
+			"pair": "DMD/BTC",
+			"q": 18.31132894
+		},
+		{
+			"title": "MOIN (Moin)",
+			"symbol": "MOIN",
+			"api": "livecoin",
+			"pair": "MOIN/BTC",
+			"q": 420.49591867
+		}
+	]
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const ccxt = require ('ccxt/ccxt.js');
 const express = require('express')
 const path = require('path')
 const app = express();
+const config = require('./config.js');
 const PORT = process.env.PORT || 5000
 
 const exchanges = [
@@ -16,8 +17,87 @@ const router = express.Router();
 
 router.get('/ticker', function (req, res) {
 
+  const securityKey = 'API_KEY';
+  let apiKey = req.get(securityKey);
+  if (!apiKey) {
+    apiKey = req.query[securityKey];
+  }
+  if (apiKey != config[securityKey]) {
+    res.status(401).end();
+  }
+
+  const btceurExchangeName = config.btceur.api;
+  const btceurPair = config.btceur.pair;
+
+  const btceurExchange = new ccxt[btceurExchangeName]({ enableRateLimit: true });
+  Promise.resolve({})
+    .then(response => {
+      return btceurExchange.fetchTicker(symbol);
+    })
+    .then(response => response['last'])
+    .then(btceur => {
+      let promises = [];
+      config.portfolio.map(port => {
+        console.log('api: ', port.api);
+        let promise = Promise
+            .resolve({})
+            .then(response => {
+              const exchange = new ccxt[port.api]({ enableRateLimit: true });
+              return exchange.fetchTicker(port.pair);
+            })
+            .then(response => {
+              return {
+                "title": port.title,
+                "datetime": response['datetime'],
+                "exchangeId": port.api,
+                "quantity": port.q,
+                "priceBtc": response['last'],
+                "priceEur": response['last'] * btceur,
+                "totalEur": response['last'] * btceur * port.q
+              };
+            });
+
+        promises.push(promise);
+      });
+      return Promise
+        .all(promises)
+        .then(response => {
+          let totalBtc = 0.0;
+          let totalEur = 0.0;
+          response.map(r => {
+            totalBtc = totalBtc + (r['priceBtc'] * r['quantity']);
+            totalEur = totalEur + (r['totalEur']);
+          });
+
+          response.map(r => {
+            r['percentage'] = r['totalEur'] * 100 / totalEur;
+          });
+
+          // Hack
+          // price Eur contains the totalEur
+          // TotalEur contains the Gain over invested sum in Eur
+          response.push({
+            "title": "Grand Total",
+            "datetime": Date.now(),
+            "exchangeId": "---",
+            "percentage": 100.0,
+            "priceBtc": totalBtc,
+            "priceEur": totalEur,
+            "totalEur": totalEur - config.investment
+          });
+          return response;
+        });
+    })
+    .then(response => res.json(response))
+    .catch(error => {
+      console.error(error);
+      res.status(504).end();
+    });
+
+  //console.log(config);
   //const exchangeId = exchanges[0];
 
+  /*
   let promises = [];
   exchanges.map(exchangeId => {
     let promise = Promise
@@ -43,46 +123,7 @@ router.get('/ticker', function (req, res) {
     .catch(error => {
       console.error(error);
     });
-
-  /*
-  new Promise((resolve, reject) => {
-    exchange.fetchTicker(symbol)
-    resolve({
-      "datetime": ticker['datetime'],
-      "exchangeId": exchangeId,
-      "price": ticker['last']
-    });
-  }).then(response => res.send(response));
-  */
-
-/*
-  await Promise.all (exchanges.map (exchangeId =>
-
-      new Promise (async (resolve, reject) => {
-
-          const exchange = new ccxt[exchangeId] ({ enableRateLimit: true })
-
-          //while (true) {
-
-              const ticker = await exchange.fetchTicker (symbol)
-              tickers[exchangeId] = ticker;
-
-              Object.keys (tickers).map (exchangeId => {
-                  const ticker = tickers[exchangeId]
-                  //console.log (ticker['datetime'], exchangeId, ticker['last']);
-                  resolve({
-                    "datetime": ticker['datetime'],
-                    "exchangeId": exchangeId,
-                    "price": ticker['last']
-                  })
-              })
-          //}
-
-      })
-
-  )).then(response => res.send(response))
-*/
-
+    */
 });
 
 app
