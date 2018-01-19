bfce40bb1e52311a293eb664171301e0975de6a0
chefbook recipes <chefbook.recipes@gmail.com>
2018-01-19 14:31:23 -0800

    update

--- config.js
+++ config.js
@@ -19,35 +19,21 @@ module.exports =
 			"symbol": "XRP",
 			"api": "bitfinex",
 			"pair": "XRP/BTC",
-			"q": 699.2424392
-		},
-		{
-			"title": "IOTA (Iota)",
-			"symbol": "IOTA",
-			"api": "bitfinex",
-			"pair": "IOTA/BTC",
-			"q": 67.06108908
-		},
-		{
-			"title": "ARK (Ark)",
-			"symbol": "ARK",
-			"api": "binance",
-			"pair": "ARK/BTC",
-			"q": 76.58000000
+			"q": 639.2424392
 		},
 		{
 			"title": "DMD (Diamond)",
 			"symbol": "DMD",
 			"api": "livecoin",
 			"pair": "DMD/BTC",
-			"q": 19.83000000
+			"q": 21.60000000
 		},
 		{
 			"title": "MOIN (Moin)",
 			"symbol": "MOIN",
 			"api": "livecoin",
 			"pair": "MOIN/BTC",
-			"q": 424.89344680
+			"q": 1976.06407678
 		}
 	]
 }
